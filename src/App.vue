eae1fa1cb2cdb55139983b2b47f6e67cd64543c2
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-01-01 21:48:36 +0330

    designed kanban board sidebar using tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,80 @@
+---
+uuid: base
+name: Kanban Board
+published_at: null
+category: Page Layouts
+subcategory: Application UI
+---
+
 <template>
-  <div id="app" class="bg-gray-100 font-sans w-full min-h-screen m-0">
-    <div class="bg-white shadow">
-      <div class="container mx-auto px-4">
-        <div class="flex items-center justify-between py-4">
-          <div>
-            <img alt="Vue logo" src="@/assets/logo.png" class="w-10 h-10" />
-          </div>
-
-          <div class="hidden sm:flex sm:items-center">
-            <router-link
-              to="/"
-              class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4"
-              >Home</router-link
-            >
-            <router-link
-              to="/about"
-              class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4"
-              >About</router-link
-            >
-          </div>
-
-          <div class="hidden sm:flex sm:items-center">
-            <a
-              href="#"
-              class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4"
-              >Sign in</a
-            >
-            <a
-              href="#"
-              class="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600"
-              >Sign up</a
-            >
-          </div>
+  <div class="h-screen flex">
+    <div class="w-64 px-8 py-4 bg-gray-100 border-r border-r">
+      <img class="h-8 w-8" src="@/assets/logo.png" alt="vuejs logo" />
+      <nav class="mt-8">
+        <h2 class="text-xs font-semibold text-gray-600 uppercase tracking-wide">
+          Issues
+        </h2>
+        <div class="mt-2 -mx-3">
+          <a
+            href="#"
+            class="flex justify-between items-center px-3 py-2 bg-gray-200 rounded-lg"
+          >
+            <p class="text-sm font-medium text-gray-900">All</p>
+            <span class="text-xs font-semibold text-gray-700">36</span>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Assigned to me</p>
+            <span class="text-xs font-semibold text-gray-700">2</span>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Created by me</p>
+            <span class="text-xs font-semibold text-gray-700">1</span>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Archived</p>
+            <span class="text-xs font-semibold text-gray-700"></span>
+          </a>
         </div>
-      </div>
+
+        <h2 class="mt-8 text-xs font-semibold text-gray-600 uppercase tracking-wide">
+          Tags
+        </h2>
+        <div class="mt-2 -mx-3">
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Bug</p>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Feature Request</p>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Marketing</p>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">v2.0</p>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Enhancement</p>
+          </a>
+          <a href="#" class="flex justify-between items-center px-3 py-2 rounded-lg">
+            <p class="text-sm font-medium text-gray-700">Design</p>
+          </a>
+        </div>
+        <button class="mt-2 -ml-2 flex items-center text-sm font-medium text-gray-600">
+          <svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
+            <path
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              d="M12 7v10m5-5H7"
+            />
+          </svg>
+          <span class="ml-1">New Project</span>
+        </button>
+      </nav>
     </div>
-    <router-view />
+    <div class="flex-1 min-w-0 bg-white">
+      <div class="border-b-2 border-gray-200"></div>
+      <div></div>
+    </div>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
