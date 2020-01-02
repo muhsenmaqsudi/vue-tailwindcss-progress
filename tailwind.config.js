c5c86268787eca629b5d35a8ff9e9581e25165a6
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-01-02 18:08:16 +0330

    installed tailwindcss-animations plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -471,6 +471,77 @@ module.exports = {
       l: 'left',
       tl: 'top left',
     },
+    // tailwindcss-animations plugin
+    animations: {
+      spin: {
+        from: {
+          transform: 'rotate(0deg)',
+        },
+        to: {
+          transform: 'rotate(360deg)',
+        },
+      },
+      jump: {
+        '0%': {
+          transform: 'translateY(0%)',
+        },
+        '50%': {
+          transform: 'translateY(-100%)',
+        },
+        '100%': {
+          transform: 'translateY(0%)',
+        },
+      },
+    },
+    animationDuration: {
+      default: '1s',
+      '0s': '0s',
+      '1s': '1s',
+      '2s': '2s',
+      '3s': '3s',
+      '4s': '4s',
+      '5s': '5s',
+    },
+    animationTimingFunction: {
+      default: 'ease',
+      linear: 'linear',
+      ease: 'ease',
+      'ease-in': 'ease-in',
+      'ease-out': 'ease-out',
+      'ease-in-out': 'ease-in-out',
+    },
+    animationDelay: {
+      default: '0s',
+      '0s': '0s',
+      '1s': '1s',
+      '2s': '2s',
+      '3s': '3s',
+      '4s': '4s',
+      '5s': '5s',
+    },
+    animationIterationCount: {
+      default: 'infinite',
+      once: '1',
+      infinite: 'infinite',
+    },
+    animationDirection: {
+      default: 'normal',
+      normal: 'normal',
+      reverse: 'reverse',
+      alternate: 'alternate',
+      'alternate-reverse': 'alternate-reverse',
+    },
+    animationFillMode: {
+      default: 'none',
+      none: 'none',
+      forwards: 'forwards',
+      backwards: 'backwards',
+      both: 'both',
+    },
+    animationPlayState: {
+      running: 'running',
+      paused: 'paused',
+    },
   },
   variants: {
     accessibility: ['responsive', 'focus'],
@@ -551,11 +622,21 @@ module.exports = {
     transformStyle: ['responsive'],
     backfaceVisibility: ['responsive'],
     transformBox: ['responsive'],
+    // animations plugin
+    animations: ['responsive'],
+    animationDuration: ['responsive'],
+    animationTimingFunction: ['responsive'],
+    animationDelay: ['responsive'],
+    animationIterationCount: ['responsive'],
+    animationDirection: ['responsive'],
+    animationFillMode: ['responsive'],
+    animationPlayState: ['responsive'],
   },
   corePlugins: {},
   plugins: [
     require('tailwindcss-transforms')({
       '3d': false,
     }),
+    require('tailwindcss-animations')(),
   ],
 };
